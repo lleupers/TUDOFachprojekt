2bdad4a5205b54baa5218b09f693f03f7d281f29
Lorenz Leupers <lorenz.leupers@tu-dortmund.de>
2025-11-19 20:37:20 +0100

    Tried Week5/C

--- Week5/C.py
+++ Week5/C.py
@@ -6,21 +6,21 @@ secondFactor = secondNumber.pop(0)
 resultFactor = result.pop(0)
 
 min_base = max(max(firstNumber), max(secondNumber), max(result)) + 1
-max_base = 2073741825 # i chose this.
+max_base = 1073751825 # i chose this.
 
 for base in range(min_base, max_base):
     firstValue = 0
     secondValue = 0
     resultValue = 0
 
-    for i in range(firstFactor):
-        firstValue += firstNumber[i] * (base ** (firstFactor - i - 1))
+    for d in firstNumber:
+        firstValue = firstValue * base + d
 
-    for i in range(secondFactor):
-        secondValue += secondNumber[i] * (base ** (secondFactor - i - 1))
+    for d in secondNumber:
+        secondValue = secondValue * base + d
 
-    for i in range(resultFactor):
-        resultValue += result[i] * (base ** (resultFactor - i - 1))
+    for d in result:
+        resultValue = resultValue * base + d
 
     if resultValue == firstValue * secondValue:
         print(base)
