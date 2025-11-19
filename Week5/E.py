3fcf9cd91a5a240b6d74e1c53688618292ee47d8
Lorenz <lorenzleupers@gmail.com>
2025-11-19 14:46:01 +0100

    Tried Week5/E

--- Week5/E.py
+++ Week5/E.py
@@ -3,5 +3,23 @@ height = numbers[0]
 width = numbers[1]
 length = numbers[2]
 
-placed = list(map(int,input().split(" ")))
+matrix = [[0]*height for _ in range(width)]
 
+placed = list(map(int,input().split(" ")))
+# A equals 1, B equals 2
+
+
+
+for p, currentCol in enumerate(placed):
+    col = currentCol - 1
+    if p % 2 == 0:
+        player = 1
+    else:
+        player = 2
+
+    for row in range(height):
+        if matrix[col][row] == 0:
+            matrix[col][row] = player
+            break
+    
+    
