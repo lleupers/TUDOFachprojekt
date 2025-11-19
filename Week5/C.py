e3e8fd5c41ef4c66431a9150a1aa36b06ae4ddbb
Lorenz Leupers <lorenz.leupers@tu-dortmund.de>
2025-11-19 20:03:46 +0100

    Worked on Week5/C

--- Week5/C.py
+++ Week5/C.py
@@ -1,10 +1,29 @@
 firstNumber = list(map(int,input().split(" ")))
 secondNumber = list(map(int,input().split(" ")))
 result = list(map(int,input().split(" ")))
+firstFactor = firstNumber.pop(0)
+secondFactor = secondNumber.pop(0)
+resultFactor = result.pop(0)
 
-min_base = max(max(firstNumber[1:]), max(secondNumber[1:]), max(result[1:])) + 1
-max_base = 256 # i chose this.
-print(min_base, max_base)
+min_base = max(max(firstNumber), max(secondNumber), max(result)) + 1
+max_base = 2073741825 # i chose this.
 
 for base in range(min_base, max_base):
-    
+    firstValue = 0
+    secondValue = 0
+    resultValue = 0
+
+    for i in range(firstFactor):
+        firstValue += firstNumber[i] * (base ** (firstFactor - i - 1))
+
+    for i in range(secondFactor):
+        secondValue += secondNumber[i] * (base ** (secondFactor - i - 1))
+
+    for i in range(resultFactor):
+        resultValue += result[i] * (base ** (resultFactor - i - 1))
+
+    if resultValue == firstValue * secondValue:
+        print(base)
+        exit()
+
+print("impossible")
